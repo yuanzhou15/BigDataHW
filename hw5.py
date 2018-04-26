0fb1e9d683526ba514fe76f6ac2e37fab2737477
Yuan Zhou <yuanzhou719@gmail.com>
2018-04-26 14:14:19 -0400

    final

--- hw5.py
+++ hw5.py
@@ -1,8 +1,5 @@
-sc
-NYC_REST = 'nyc_restaurants.csv'
-rest = sc.textFile(NYC_REST, use_unicode=False).cache()
-rest.take(3)
-list(enumerate(rest.first().split(',')))
+import sys
+from pyspark import SparkContext as sc
 
 def extractCuisine(partId, list_of_records):
     if partId==0: 
@@ -12,12 +9,20 @@ def extractCuisine(partId, list_of_records):
     for row in reader:
         cuisine = row[7]
         yield(cuisine)
-        
 
-allCuisines = rest.mapPartitionsWithIndex(extractCuisine)
-allCuisines.take(10)
+def main(sc):
+	NYC_REST = '/data/share/bdm/nyc_restaurants.csv'
+	rest = sc.textFile(NYC_REST, use_unicode=False).cache()
+	rest.take(3)
+	list(enumerate(rest.first().split(',')))
+	allCuisines = rest.mapPartitionsWithIndex(extractCuisine)
+	allCuisines.take(10)
 
-file = open("output.txt", "w")
-for k in allCuisines.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y).collect():
+	file = open("output.txt", "w")
+	for k in allCuisines.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y).collect():
     file.write(str(k) + "\n")
-file.close()
+	file.close()
+
+if __name__ == '__main__':
+	sc = SparkContext()
+	main(sc)
